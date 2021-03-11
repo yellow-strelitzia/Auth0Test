9ccbbaeac0eb88c8bd91465aa627b8f82c7c0035
yellow-strelitzia <kunigogo.dev.public@gmail.com>
2021-03-11 13:31:49 +0000

    modified

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const createClient = async () => {
-  const auth0 = await createAuth0Client({
+  let auth0 = await createAuth0Client({
     domain: 'kunigogo-dev-public.us.auth0.com',
     client_id: '0gs9YSsO4Bku00fzgke6jd7mi4AANhTj'
   });
