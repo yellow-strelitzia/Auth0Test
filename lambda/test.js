60f3f821a6c2257b1b06bdbda4b53baff3c178b3
yellow-strelitzia <kunigogo.dev.public@gmail.com>
2021-03-12 17:49:13 +0000

    using serverless-jwt

--- lambda/test.js
+++ lambda/test.js
@@ -1,8 +1,22 @@
-const auth0 = require("auth0");
+const { NetlifyJwtVerifier } = require('@serverless-jwt/netlify');
 
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify("Hello, world!")
-  });
-};
+const requireAuth = NetlifyJwtVerifier({
+  issuer: process.env.JWT_ISSUER,
+  audience: process.env.JWT_AUDIENCE
+});
+
+exports.handler = requireAuth(async (event, context) => {
+  try {
+    const { claims } = context.identityContext;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ claims })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error_description: err.message })
+    };
+  }
+});
